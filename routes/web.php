2bf383b9d06c756782e80da7090c0448e800db27
zhouqiongyao <zhouqiongyao@huoyunren.com>
2017-11-28 18:02:36 +0800

    push laravel-framework

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','TestController@index', function () {
+    //return view('index',['name' => 'xzz']);
+});
+/*Route::get('/', function () {
+    return redirect()->action('TestController@test'); // redirect的必需已经定义过路由
+});*/
+/*Route::get('/', function () {
+    return redirect('test'); // redirect的必需已经定义过路由
+});*/
+Route::any('test','TestController@test', function () {
+    return 'sdfdsfdsf';
+});
+Route::any('synclg','TestController@getLgPositions', function () {
+    //
+});
+/*Route::get('/test', function () {
+    return view('test');
+});*/
